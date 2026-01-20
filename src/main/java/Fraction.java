d854b0fd3bcb7ee5ca5d5df3854a6007e49ddd3b
Anna Memikova <anna.memikova@yandex.ru>
2026-01-20 18:41:29 +0300

    Практика ООП. Инкапсуляция №1. Дробь

---
+++ src/main/java/Fraction.java
@@ -0,0 +1,44 @@
+public class Fraction {
+    private final int numerator;
+    private final int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        if (denominator < 0)
+            throw new IllegalArgumentException("denominator must be positive");
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction sum(Fraction secondFraction) {
+        int newNumerator = this.numerator * secondFraction.denominator
+                + secondFraction.numerator * this.denominator;
+        int newDenominator = this.denominator * secondFraction.denominator;
+
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction sum(int number) {
+        int newNumerator = this.numerator + number * this.denominator;
+
+        return new Fraction(newNumerator, denominator);
+    }
+
+    public Fraction minus(Fraction secondFraction) {
+        int newNumerator = this.numerator * secondFraction.denominator
+                - secondFraction.numerator * this.denominator;
+        int newDenominator = this.denominator * secondFraction.denominator;
+
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction minus(int number) {
+        int newNumerator = this.numerator - number * this.denominator;
+
+        return new Fraction(newNumerator, denominator);
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,9 @@
-import java.io.File;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int j = 0;
-        while (true) {
-            String path = new Scanner(System.in).nextLine();
-            File file = new File(path);
-            boolean fileExists = file.exists();
-            boolean isDirectory = file.isDirectory();
-            if (isDirectory || !fileExists) {
-                if (!fileExists) {
-                    System.out.println("Файл не существует");
-                } else {
-                    System.out.println("Указанный путь не ведет к файлу");
-                }
-                continue;
-            } else
-                j++;
-            System.out.println("Путь указан верно");
-            System.out.println("Это файл номер " + j);
-        }
+        Fraction f1 = new Fraction(1, 3);
+        Fraction f2 = new Fraction(2, 5);
+        Fraction f3 = new Fraction(7, 8);
+
+        System.out.println(f1.sum(f2).sum(f3).minus(5));
     }
 }
