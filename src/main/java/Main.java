459f7f20c03c4d85ee91caf70b43e892b0adea76
Anna Memikova <anna.memikova@yandex.ru>
2026-01-29 23:18:38 +0300

    Практика ООП. Полиморфизм №5. Дробь это число

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Fraction f1 = new Fraction(1, 3);
-        Fraction f2 = new Fraction(2, 5);
-        Fraction f3 = new Fraction(7, 8);
 
-        System.out.println(f1.sum(f2).sum(f3).minus(5));
+        System.out.println(f1.intValue());
+        System.out.println(f1.longValue());
+        System.out.println(f1.floatValue());
+        System.out.println(f1.doubleValue());
     }
 }
