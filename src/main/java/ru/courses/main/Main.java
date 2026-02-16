b1ac30608569cba66b207e92b2c7dc846a5d46ed
Anna Memikova <anna.memikova@yandex.ru>
2026-02-16 22:19:31 +0300

    Задание 2. Сравнение объектов

--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -26,32 +26,72 @@ public class Main {
             System.out.println("Это файл номер " + j);
 
             try {
-                int linesCount = 0;
-                int maxLength = 0;
-                int minLength = Integer.MAX_VALUE;
-
                 FileReader fileReader = new FileReader(path);
                 BufferedReader reader = new BufferedReader(fileReader);
                 String line;
 
+                int totalRequests = 0;
+                int googleCount = 0;
+                int yandexCount = 0;
+
                 while ((line = reader.readLine()) != null) {
                     int length = line.length();
                     if (length > 1024) {
-                        throw new RuntimeException(
-                                "Обнаружена строка длиннее 1024 символов");
+                        throw new RuntimeException("Обнаружена строка длиннее 1024 символов");
                     }
-                    linesCount++;
-                    if (length > maxLength) {
-                        maxLength = length;
+
+                    totalRequests++;
+
+                    int lastQuote = line.lastIndexOf("\"");
+                    int prevQuote = line.lastIndexOf("\"", lastQuote - 1);
+
+                    if (prevQuote == -1 || lastQuote == -1) {
+                        continue;
                     }
-                    if (length < minLength) {
-                        minLength = length;
+
+                    String userAgent =
+                            line.substring(prevQuote + 1, lastQuote);
+
+                    int open = userAgent.indexOf("(");
+                    int close = userAgent.indexOf(")");
+
+                    if (open == -1 || close == -1) {
+                        continue;
+                    }
+
+                    String firstBrackets = userAgent.substring(open + 1, close);
+
+                    String[] parts = firstBrackets.split(";");
+
+                    if (parts.length < 2) {
+                        continue;
+                    }
+
+                    String fragment = parts[1].trim();
+
+                    int slashIndex = fragment.indexOf("/");
+                    if (slashIndex != -1) {
+                        fragment = fragment.substring(0, slashIndex);
+                    }
+
+                    if (fragment.equals("Googlebot")) {
+                        googleCount++;
+                    } else if (fragment.equals("YandexBot")) {
+                        yandexCount++;
                     }
                 }
-                reader.close();
-                System.out.println("общее количество строк в файле = " + linesCount);
-                System.out.println("длина самой длинной строки в файле = " + maxLength);
-                System.out.println("длина самой короткой строки в файле = " + minLength);
+
+                double googleShare =
+                        totalRequests == 0 ? 0 :
+                                (double) googleCount / totalRequests;
+
+                double yandexShare =
+                        totalRequests == 0 ? 0 :
+                                (double) yandexCount / totalRequests;
+
+                System.out.println("Всего запросов: " + totalRequests);
+                System.out.println("Googlebot доля: " + googleShare);
+                System.out.println("YandexBot доля: " + yandexShare);
             } catch (Exception e) {
                 e.printStackTrace();
             }
